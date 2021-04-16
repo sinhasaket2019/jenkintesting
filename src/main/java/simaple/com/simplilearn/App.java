64322bfde6dbbcb6b07a740643695b5b42766e03
Ubuntu <sinhasaket2019@gmail.com>
2021-04-16 08:43:29 +0000

    adding new project for testing end to end jenkins

---
+++ src/main/java/simaple/com/simplilearn/App.java
@@ -0,0 +1,13 @@
+package simaple.com.simplilearn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
